bbbb2713b56fffeaf73f6a13094d63e47076e0c6
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-12-11 17:40:10 -0500

    Add simple connect and query example
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/main/java/io/github/mapepire_ibmi/App.java
+++ src/main/java/io/github/mapepire_ibmi/App.java
@@ -1,17 +1,34 @@
 package io.github.mapepire_ibmi;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public final class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            MapepireDriver mapepireDriver = new MapepireDriver();
+            DriverManager.registerDriver(mapepireDriver);
+
+            final Properties p = new Properties();
+            p.put("USER", "USER");
+            p.put("PASSWORD", "PASSWORD");
+            p.put("naming", "system");
+            p.put("errors", "full");
+
+            Connection connection = DriverManager.getConnection("jdbc:mapepire://HOST:PORT", p);
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM SAMPLE.DEPARTMENT");
+            rs.next();
+            String DEPTNO = rs.getString(1);
+
+            connection.close();
+            System.out.println("Done");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
